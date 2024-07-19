6db02cbf7ee77a77bbf585c4162c2db306f67029
Bijay Magar <bijaym095@gmail.com>
2024-07-19 21:45:46 +0545

    Initial setup and added global stylings

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+import RootLayout from "@/layouts/RootLayout";
+import Home from "@/pages/Home";
+import About from "@/pages/About";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+    </Route>
+  )
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "16px",
+      },
+
+      colors: {
+        primary: {
+          DEFAULT: "#ee626b",
+        },
+        secondary: {
+          DEFAULT: "#0071F8",
+        },
+        "lx-gray": {
+          DEFAULT: "#eeeeee",
+        },
+      },
+
+      screens: {
+        sm: "576px",
+        md: "768px",
+        lg: "992px",
+        xl: "1200px",
+        "2xl": "1310px",
+      },
+
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
